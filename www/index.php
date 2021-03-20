0495dd3c810d7dcdc8938b68b2b0b438d670c3d6
gian_tlcws <g.bennati@tlcws.com>
2021-03-20 10:12:20 +0100

    project init

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&amp;display=swap" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+    <title>Test Landing</title>
+</head>
+<body>
+    <!-- WRAP 2560px -->
+    <div class="site-wrap">
+        <!-- WRAP 1920px -->
+        <div class="site-wrap__contents">
+
+
+        </div>
+        <!-- WRAP 1920px / -->
+    </div>
+    <!-- WRAP 2560px / -->
+    <script src="assets/js/script.min.js"></script>
+</body>
+</html>
